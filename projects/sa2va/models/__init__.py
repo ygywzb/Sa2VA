e70944a5ceffacef5a45ee278003a911895c1359
Anaaya <Anaaya949967@gmail.com>
2026-01-27 16:07:12 +0800

    添加CUDA路径到调试配置，修改mllm适配文件名错误

--- projects/sa2va/models/__init__.py
+++ projects/sa2va/models/__init__.py
@@ -4,6 +4,12 @@ from .sam2_train import SAM2TrainRunner
 from .preprocess import DirectResize
 
 from .mllm.internvl import InternVLMLLM
-from .mllm.intervl_train import InternVLMLLM_Train
+from .mllm.internvl_train import InternVLMLLM_Train
 
-__all__ = ['Sa2VAModel', 'SAM2TrainRunner', 'DirectResize', 'InternVLMLLM', 'InternVLMLLM_Train']
+__all__ = [
+    "Sa2VAModel",
+    "SAM2TrainRunner",
+    "DirectResize",
+    "InternVLMLLM",
+    "InternVLMLLM_Train",
+]
