611517411401054cfee6bb9eb795335beab3b698
anaaya_hyy <Anaaya949967@gmail.com>
2026-04-20 18:17:07 +0800

    更改MMMU评估md5值，添加调试MMMU配置

--- sa2va_eval/vlmeval/dataset/image_mcq.py
+++ sa2va_eval/vlmeval/dataset/image_mcq.py
@@ -266,7 +266,8 @@ class MMMUDataset(ImageMCQDataset):
     }
 
     DATASET_MD5 = {
-        'MMMU_DEV_VAL': '521afc0f3bf341e6654327792781644d',
+        # 'MMMU_DEV_VAL': '521afc0f3bf341e6654327792781644d', # old md5
+        'MMMU_DEV_VAL':'585e8ad75e73f75dcad265dfd0417d64',
         'MMMU_TEST': 'c19875d11a2d348d07e5eb4bdf33166d',
     }
 
